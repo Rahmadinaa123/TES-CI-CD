a039835f49090cecf4efe1c769e5f07ceba54b94
Rahmadinaa123 <rahmadinaa2002@gmail.com>
2024-08-11 15:10:15 +0700

    user

--- resources/views/admin/dataObat/tambahdataObat.blade.php
+++ resources/views/admin/dataObat/tambahdataObat.blade.php
@@ -60,7 +60,7 @@
             <li class="nav-item active">
                 <a class="nav-link" href="{{ route('admin.user') }}">
                     <i class="fas fa-fw fa-user"></i>
-                    <span>User name</span></a>
+                    <span>User2</span></a>
             </li>
 
             <li class="nav-item">
