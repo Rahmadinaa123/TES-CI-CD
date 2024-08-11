61691fa2127571cd84d3547ab454d77a847b572b
ratih <ratihnoviyanti211103@gmail.com>
2024-08-11 14:26:29 +0700

    gh

--- resources/views/admin/dataObat/dataObat.blade.php
+++ resources/views/admin/dataObat/dataObat.blade.php
@@ -45,7 +45,7 @@
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('admin.home') }}">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Berandaa</span></a>
+                    <span>Beranda</span></a>
             </li>
 
             <!-- Divider -->
