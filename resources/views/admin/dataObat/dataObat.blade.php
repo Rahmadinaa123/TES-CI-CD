89ea3959c10be34280089fe481f2852fc72ac23e
ratih <ratihnoviyanti211103@gmail.com>
2024-08-11 14:36:42 +0700

    gh

--- resources/views/admin/dataObat/dataObat.blade.php
+++ resources/views/admin/dataObat/dataObat.blade.php
@@ -35,7 +35,7 @@
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">SMART FARMA tes</sup></div>
+                <div class="sidebar-brand-text mx-3">SMART FARMA tess</sup></div>
             </a>
 
             <!-- Divider -->
