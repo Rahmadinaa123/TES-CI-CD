20aad934f6b215de4f6c82dc62fa38f0e7f263f9
Rahmadinaa123 <rahmadinaa2002@gmail.com>
2024-09-21 19:55:44 +0700

    ubah obat

--- resources/views/admin/dataObat/tambahdataObat.blade.php
+++ resources/views/admin/dataObat/tambahdataObat.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Tambah Data Obat</title>
+    <title>Tambah Obat</title>
 
     <!-- Custom fonts for this template-->
     <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
